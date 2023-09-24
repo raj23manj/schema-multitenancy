b5119b3f344a2ff1e5221f3cea277c32c391c61a
rajesh <rajesh.rajsrv@gmail.com>
2023-09-24 10:17:13 +0530

    first commit

---
+++ src/main/java/com/schema/multinenancy/MultinenancyApplication.java
@@ -0,0 +1,13 @@
+package com.schema.multinenancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultinenancyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultinenancyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schema/multinenancy/MultinenancyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schema.multinenancy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultinenancyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
